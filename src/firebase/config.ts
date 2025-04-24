546301710627279f8bf3c7ebe11417a9572ec467
David Lopez <cristiandml3722@gmail.com>
2025-04-24 00:54:39 -0500

    Final review for future revisions

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,21 +5,6 @@ import { firebaseConfig } from "./key";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBCwl6JatmYrQIRNkLdz1vTe_8-m845I7A",
-
-//   authDomain: "astro-auth-5cc4f.firebaseapp.com",
-
-//   projectId: "astro-auth-5cc4f",
-
-//   storageBucket: "astro-auth-5cc4f.firebasestorage.app",
-
-//   messagingSenderId: "180284056144",
-
-//   appId: "1:180284056144:web:0e8592b5e1e51e5abbe755",
-// };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
