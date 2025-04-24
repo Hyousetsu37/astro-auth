37ee3fea29466f726ce2598f0a979c23dca66c56
David Lopez <cristiandml3722@gmail.com>
2025-04-24 00:28:13 -0500

    third commit

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,14 +5,6 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBH3fKGO5EVjlzY0vJGChNU405e4nQeibI",
-  authDomain: "astro-auth-61e2b.firebaseapp.com",
-  projectId: "astro-auth-61e2b",
-  storageBucket: "astro-auth-61e2b.firebasestorage.app",
-  messagingSenderId: "1023681240268",
-  appId: "1:1023681240268:web:855bfd9bc02c8866f77669",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
